75327013c3ec542264d541ba4a1b840313ad2a7e
owusuvictorjnr <owusuvictor80@gmail.com>
2022-12-19 08:21:48 -0500

    Create Website Layout

--- pages/README.md
+++ pages/README.md
@@ -4,3 +4,11 @@
 
 1. Create Next app
 2. Install Tailwindcss
+
+3.Create Website Layout
+
+   1. create layout component
+   2. add header
+   3. add main section
+   4. add footer
+   5. add tailwindcss classes
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -14,9 +13,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=''>
-        
-      </main>
     </>
-  )
+  );
 }
